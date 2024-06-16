22b268f303216ab5ae126dc77b6564a366770c6e
chengivan <chengoleivan@gmail.com>
2024-06-16 23:14:30 +0600

    Installed @vercel/analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react"
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -9,8 +10,10 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+
   return (
     <html lang="en">
+      <Analytics/>
       <body className={inter.className}>{children}</body>
     </html>
   );
