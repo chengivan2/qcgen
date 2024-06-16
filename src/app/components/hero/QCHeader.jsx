fb2e2ebc72dfa7bc68a34671396c2fbb86746bb0
chengivan <chengoleivan@gmail.com>
2024-06-16 17:33:08 +0600

    Styled the header, deleted some unnecessary static files and added VSCode folders and files to .gitignore

--- src/app/components/hero/QCHeader.jsx
+++ src/app/components/hero/QCHeader.jsx
@@ -1,14 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
 import qcGenLogo from "../../../../public/qcgenlogo.png";
-import '@/app/componentsstyles/herostyles/QCHeader.css'
+import "@/app/componentsstyles/herostyles/QCHeader.css";
 
 export const QCHeader = () => {
   return (
     <div className="qc-header-container">
-
       <div className="qclogo-container">
-        <Image className="qcgen-generated-qr-code-image" src={qcGenLogo} width={160} height={160} alt="QC Gen Header Logo" placeholder="blur" />
+        <Image
+          className="qcgen-generated-qr-code-image"
+          src={qcGenLogo}
+          width={96}
+          height={96}
+          alt="QC Gen Header Logo"
+          placeholder="blur"
+        />
       </div>
 
       <div className="qcheader-menu-container">
@@ -18,7 +24,6 @@ export const QCHeader = () => {
           <Link href="https://ivanthedev.guru">ivanthedev</Link>
         </nav>
       </div>
-
     </div>
   );
 };
